670d53a464a7afea9e024d9cde262292ad513af6
richard <richard.lee10@myhunter.cuny.edu>
2018-09-13 17:12:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,60 @@
+var uppercase="HELLO!";
+var lowercase="hello!";
+
+//function 7
+function sayHiToGrandma(string){
+  if(string==="I love you, Grandma.")
+  {
+      return "I love you, too.";
+  }
+
+}
+
+
+//function 6
+function sayHiToGrandma(string){
+  if(string===string.toUpperCase())
+  {
+    string3='YES INDEED!';
+      return string3;
+  }
+
+}
+
+//function 5
+function sayHiToGrandma(string){
+  if(string===string.toLowerCase())
+  {
+      return 'I can\'t hear you!';
+  }
+}
+
+
+//function 4
+function logWhisper(string){
+  if(string===string.toLowerCase())
+  {
+      console.log(lowercase);
+  }
+
+}
+
+var uppercase = "HELLO!";
+
+//function 3
+function logShout(uppercase){
+  if(uppercase.toUpperCase()===uppercase)
+      console.log(uppercase.toUpperCase());
+}
+
+
+//function 2
+function whisper(string){
+  return string.toLowerCase();
+}
+
+
+//function 2
+function shout(string){
+  return string.toUpperCase();
+}
